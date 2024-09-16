89c455122ce2c599380285be4bd578d5a9791648
chris-wtly <christianwhitley19@outlook.com>
2024-09-16 15:52:54 +0100

    task 6 completed - needs styling

--- src/components/ArticleContainer.jsx
+++ src/components/ArticleContainer.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import { fetchArticles } from "../requests";
 import { ArticleCard } from "./ArticleCard";
+import { Link } from "react-router-dom";
 
 export function ArticleContainer() {
   const [articleCards, setArticleCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchArticles().then(({ data: { articles } }) => {
       setArticleCards(articles);
+      setIsLoading(false);
     });
   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return <ArticleCard articleCards={articleCards} />;
 }
--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,28 +1,35 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchArticle } from "../requests";
+import { Comments } from "./Comments";
 
 export function ArticlePage() {
   const { article_id } = useParams();
 
   const [articleData, setArticleData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchArticle(article_id).then(({ data: { articles } }) => {
       setArticleData(articles);
+      setIsLoading(false);
     });
   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <div className="article_page">
         <h1>{articleData.title}</h1>
         <h2>{articleData.author}</h2>
         <h3>{articleData.topic}</h3>
-        <img src={articleData.article_img_url} />
-        <article>{articleData.body}</article>
+        <img className="img_card" src={articleData.article_img_url} />
+        <article className="article">{articleData.body}</article>
         <button>Like {articleData.votes}</button>
-        <button>Comments {articleData.comment_count}</button>
       </div>
+      <Comments article_id={article_id} />
     </>
   );
 }
